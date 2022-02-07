d7aee00785d121f2c791cb6749461d675d7fab3d
LuckyShmucky <christianluciano567@gmail.com>
2022-02-07 13:01:02 -0800

    completed admin portal functionality

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,35 @@
+async function main(){
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(displayBooks)
+    console.log(books)
+}
 
-// Your Code Here
+
+function displayBooks(books){
+    let root = document.querySelector('#root')
+    let li = document.createElement('li')
+    li.textContent = books.title
+    let amountOfBooks = document.createElement('input')
+    amountOfBooks.value = books.quantity
+    let submitButton = document.createElement('button')
+    submitButton.textContent = 'Save'
+
+    submitButton.addEventListener('click', function(){
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: books.id,
+                quantity: amountOfBooks.value
+            })
+        })
+    })
+
+    li.append(amountOfBooks, submitButton)
+    root.append(li)
+}
+
+main()
